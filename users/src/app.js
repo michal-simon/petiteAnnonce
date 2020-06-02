dfd8ef2869dc236013d610a374a4f13d7aa2c3a6
yecodeo <yecodeo@gmail.com>
2020-06-02 02:23:07 +0200

    create users service

---
+++ users/src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/', async (req, res) => {
+    return res.send('App working');
+});
+
+app.listen(3000, console.log(`Le serveur à démarrer sur le port ${process.env.PORT} `));
