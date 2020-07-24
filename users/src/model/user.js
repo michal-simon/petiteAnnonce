146a737680ccccd1cc9bac81f2e1779ad52943a4
Afjnik hassan <yecodeo@gmail.com>
2020-07-24 04:44:16 +0200

    Adding fields

--- users/src/model/user.js
+++ users/src/model/user.js
@@ -1,26 +1,79 @@
-import mongoose from 'mongoose';
+import mongoose, {
+    Schema
+} from 'mongoose';
 import bcrypt from 'bcrypt';
 require('mongoose-type-email');
 
 const userSchema = new mongoose.Schema({
-    civilite: Number, required: true,
-    nom: String, required: true,
-    prenom: String, required: true,
-    pseudo: String, required: true,
-    adresse: String,
-    codePostal: Number,
-    telephone: Number,
-    email: mongoose.SchemaTypes.Email, required: true,
-    password: mongoose.Mixed, required: true
+    civilite: {
+        type: Number,
+        required: true
+    },
+    nom: {
+        type: String,
+        required: true
+    },
+    prenom: {
+        type: String,
+        required: true
+    },
+    pseudo: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: mongoose.Mixed,
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+    contact: {
+        adresse: {
+            type: String
+        },
+        codePostal: {
+            type: Number
+        },
+        telephone: {
+            type: Number
+        },
+        email: {
+            type: mongoose.SchemaTypes.Email,
+            required: true,
+            unique: true
+        },
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+
+    publication: {
+        type: Schema.ObjectId,
+        ref: 'Publication'
+    },
+    details: {
+        recommandations: {
+            Type: Number,
+            default: 0
+        },
+        photo: {
+            Type: String,
+            default: 'https://www.fillmurray.com/64/64'
+        },
+    }
+
 });
-userSchema.pre('save', function  (next) {
+
+userSchema.pre('save', function (next) {
     if (this.password && this.isModified('password'));
-    bcrypt.hashSync(this.password, 10, function(err, hash){
+    bcrypt.hashSync(this.password, 10, function (err, hash) {
         this.password = hash;
     });
     next();
 });
 
-const user = mongoose.model('User', userSchema);
-
-module.exports = user;
+module.exports = mongoose.model('User', userSchema);
