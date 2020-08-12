fa98a10dea8bd01a2650a0f925da075ea53ba300
yecodeo <yecodeo@gmail.com>
2020-08-12 08:01:22 +0200

    :recycle: Refactoring Code

--- users/src/app.js
+++ users/src/app.js
@@ -3,19 +3,17 @@ import mongoose from 'mongoose';
 
 const app = express();
 
-console.log(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`)
-
-mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`, {useNewUrlParser: true, useUnifiedTopology: true });
+const urlDb = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+console.log(urlDb);
+mongoose.connect(urlDb, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erreur de connexion à la base de donnée:'));
 db.once('open', function() {
-  console.log('Connexion à la base de donnée réussi')
+  console.log('Connexion à la base de donnée réussi');
 });
 
-
-
 app.get('/', async (req, res) => {
-    return res.send('App working');
+  return res.send('App working');
 });
 
 app.listen(3000, console.log(`Le serveur à démarrer sur le port ${process.env.PORT} `));
--- users/src/utils/slugify.js
+++ users/src/utils/slugify.js
@@ -1,14 +1,15 @@
 module.exports.slugify = function slugify(string) {
-    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
-    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
-    const p = new RegExp(a.split('').join('|'), 'g')
-  
-    return string.toString().toLowerCase()
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------';
+  const p = new RegExp(a.split('').join('|'), 'g');
+
+  return string.toString().toLowerCase()
       .replace(/\s+/g, '-') // séparation par -
-      .replace(p, c => b.charAt(a.indexOf(c))) // remplacer les caractzers spéciaux
+      .replace(p, (c) => b.charAt(a.indexOf(c)))
       .replace(/&/g, '-et-') // Replace & with 'et'
-      .replace(/[^\w\-]+/g, '') // retirer tout ce qui n'est pas des lettres
-      .replace(/\-\-+/g, '-') // remplacer des - multiple par un seul
-      .replace(/^-+/, '') // Trim - début 
-      .replace(/-+$/, '') // Trim - fin
-  }
+      .replace(/[^\w-]+/g, '') // retirer tout ce qui n'est pas des lettres
+      .replace(/--+/g, '-') // remplacer des - multiple par un seul
+      .replace(/^-+/, '') // Trim - début
+      .replace(/-+$/, ''); // Trim - fin
+};
+
