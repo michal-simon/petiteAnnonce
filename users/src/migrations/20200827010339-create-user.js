8fe0e4b53f6b1fea145fa971ddb225327eeebb6b
yecodeo <yecodeo@gmail.com>
2020-08-27 03:20:50 +0200

    migration & seed

---
+++ users/src/migrations/20200827010339-create-user.js
@@ -0,0 +1,81 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      civilite: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        comment: 'Civilité'
+      },
+      nom: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        comment: 'Nom'
+      },
+      prenom: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        comment: 'Prénom'
+      },
+      pseudo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        comment: 'Pseudo'
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        min: 8,
+        max: 14,
+        comment: 'Mot de passe'
+      },
+      isActive: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        comment: 'État du compte'
+      },
+      adresse: {
+        type: Sequelize.STRING,
+        comment: 'Adresse postale'
+      },
+      codePostal: {
+        type: Sequelize.STRING,
+        comment: 'Code postale'
+      },
+      telephone: {
+        type: Sequelize.INTEGER,
+        comment: 'Téléphone'
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        comment: 'E-mail'
+      },
+      //   publications: {
+      //     type: Schema.ObjectId,
+      //     ref: 'Publication',
+      //     comment: 'Liste des annonces publiées'
+      //   },
+      recommandations: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+        comment: 'Recommandation'
+      },
+      photo: {
+        type: Sequelize.STRING,
+        defaultValue: 'https://www.fillmurray.com/64/64',
+        comment: 'Photo'
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ users/src/seeders/20200827004931-users-dummies.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users',
+      [{
+        civilite: '0',
+        nom: 'Afjnik',
+        prenom: 'Hassan',
+        pseudo: 'Yecodeo',
+        password: bcrypt.hashSync('123456', 10),
+        isActive: false,
+        adresse: '14 rue de prony',
+        codePostal: '92600',
+        telephone: '0661656000',
+        email: 'yecodeo@gmail.com',
+        recommandations: 5,
+        photo: 'https://www.fillmurray.com/64/64'
+      },
+      {
+        civilite: '1',
+        nom: 'Afjnik',
+        prenom: 'Kalvin',
+        pseudo: 'admin',
+        password: bcrypt.hashSync('123456', 10),
+        isActive: true,
+        adresse: '14 rue de prony',
+        codePostal: '92600',
+        telephone: '0661656000',
+        email: 'afjnik@gmail.com',
+        recommandations: 5,
+        photo: 'https://www.fillmurray.com/64/64'
+      }
+      ]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
