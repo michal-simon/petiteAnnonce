0a6e3109439ae2fca8808d082900dbec0b091329
yecodeo <yecodeo@gmail.com>
2020-09-08 06:12:42 +0200

    Convert to ES6

---
+++ users/src/db/index.js
@@ -0,0 +1,20 @@
+import { Sequelize, DataTypes } from 'sequelize';
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const { applyExtraSetup } = require('./extra-setup');
+
+const sequelize = new Sequelize(config);
+console.log(config);
+/* eslint global-require: "off" */
+const modelDefiners = [
+  require('./models/user.model')
+];
+
+modelDefiners.forEach((modelDefiner) => {
+  modelDefiner(sequelize, DataTypes);
+});
+
+applyExtraSetup(sequelize);
+
+module.exports = sequelize;
