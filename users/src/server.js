cafc33125015d220052031903a56fc418ac0d220
yecodeo <yecodeo@gmail.com>
2020-09-16 15:45:40 +0200

    Refactor logging

--- users/src/server.js
+++ users/src/server.js
@@ -1,6 +1,5 @@
 import app from './app';
 
 const env = process.env;
-console.info('running application');
 
 app.listen(env.APP_PORT, console.info(`🚀 serveur démarrer sur le port ${env.APP_PORT}`));
