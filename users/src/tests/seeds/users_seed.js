f910dd68cfbbfff5fd19f2ad80494c3f5e690833
yecodeo <yecodeo@gmail.com>
2020-09-16 15:47:42 +0200

    seed data base for tests

---
+++ users/src/tests/seeds/users_seed.js
@@ -0,0 +1,15 @@
+const usersSeed = {
+  civilite: 0,
+  nom: 'Afjnik',
+  prenom: 'Hassan',
+  pseudo: 'yecodeo',
+  password: '123456789#Aa',
+  isActive: false,
+  adresse: '1 rue de la paix, Asnieres sur seine',
+  codePostal: 92600,
+  telephone: '0607080910',
+  email: 'yecodeo@gmail.com',
+  recommandations: 5,
+  photo: 'https://www.fillmurray.com/64/64'
+};
+export default usersSeed;
