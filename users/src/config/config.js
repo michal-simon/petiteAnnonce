053883f903e9ada264541c00d9f2d93d5b6df03d
yecodeo <yecodeo@gmail.com>
2020-09-16 15:46:17 +0200

    Correct config to much jest

--- users/src/config/config.js
+++ users/src/config/config.js
@@ -13,7 +13,7 @@ module.exports = {
     username: env.DB_USER,
     password: env.DB_PASS,
     database: env.DB_NAME,
-    host: env.DB_HOST,
+    host: 'postgres',
     dialect: 'postgres',
     port: env.DB_PORT
   },
