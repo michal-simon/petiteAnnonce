3ac70bdbdab5f208471ce40512c35364376b963d
yecodeo <yecodeo@gmail.com>
2020-09-19 10:15:23 +0200

    test validation

--- users/src/tests/users.test.js
+++ users/src/tests/users.test.js
@@ -2,22 +2,11 @@ import request from 'supertest';
 import app from '../app';
 
 describe('test users api', () => {
-  it('should test get()', () => {
-    return new Promise((done) => {
-      request(app)
-        .get('/users/1')
-        .set('Accept', 'application/json')
-        .then((response) => {
-          expect(response.statusCode).toBe(200);
-          expect(response.body.id).toBe(1);
-          done();
-        });
-    });
-  });
-  it('should test post()', () => {
-    // avoid constraint Unique insertion
-    const suffix = Math.floor(Math.random() * 100);
+  // avoid constraint Unique insertion
+  const suffix = Math.floor(Math.random() * 100);
+  let id;
 
+  it('should test post()', () => {
     return new Promise((done) => {
       request(app)
         .post('/users/')
@@ -28,7 +17,7 @@ describe('test users api', () => {
           pseudo: `Takyori${suffix}`,
           password: '123456789aA!',
           isActive: false,
-          adresse: '22 rue des parisien',
+          adresse: '22 rue des parisiens',
           codePostal: '13013',
           telephone: '0661636585',
           email: `takyama${suffix}@gmail.com`,
@@ -40,16 +29,30 @@ describe('test users api', () => {
         .set('Accept', 'application/json')
         .then((response) => {
           expect(response.statusCode).toBe(200);
-          expect(response.body.id).toBeGreaterThan(1);
+          expect(response.body.pseudo).toBe(`Takyori${suffix}`);
+          id = response?.body?.id;
+          done();
+        });
+    });
+  });
+
+  it('should test get()', () => {
+    return new Promise((done) => {
+      request(app)
+        .get(`/users/${id}`)
+        .set('Accept', 'application/json')
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
           expect(response.body.pseudo).toBe(`Takyori${suffix}`);
           done();
         });
     });
   });
+
   it('should test put()', () => {
     return new Promise((done) => {
       request(app)
-        .put('/users/1')
+        .put(`/users/${id}`)
         .send({
           civilite: '1',
           nom: 'yannick',
@@ -64,14 +67,31 @@ describe('test users api', () => {
         });
     });
   });
-//   it('should test delete()', () => {
-//     return new Promise((done) => {
-//       request(app)
-//         .delte('/users/')
-//         .then((response) => {
-//           expect(response.statusCode).toBe(200);
-//           done();
-//         });
-//     });
-//   });
+  it('should test delete()', () => {
+    return new Promise((done) => {
+      request(app)
+        .delete(`/users/${id}`)
+        .then((response) => {
+          console.log(response.body, suffix);
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+    });
+  });
+});
+
+describe('test users api validations', () => {
+  it('should test triggers errors', () => {
+    return new Promise((done) => {
+      request(app)
+        .post('/users/')
+        .send({})
+        .set('Accept', 'application/json')
+        .then((response) => {
+          console.log(response.body);
+          expect(response.statusCode).toBe(422);
+          done();
+        });
+    });
+  });
 });
