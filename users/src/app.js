2b26b86ab58f0e3de7767b6aed17e5534fe96b55
yecodeo <yecodeo@gmail.com>
2020-09-19 23:23:09 +0200

    Add i18n

--- users/src/app.js
+++ users/src/app.js
@@ -1,7 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import i18next from 'i18next';
+import i18nextMiddleware from 'i18next-http-middleware';
+import Backend from 'i18next-fs-backend';
+
+i18next
+  .use(Backend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    // debug: true,
+    backend: {
+      // eslint-disable-next-line no-path-concat
+      loadPath: __dirname + '/locales/{{lng}}/{{ns}}.json',
+      // eslint-disable-next-line no-path-concat
+      addPath: __dirname + '/locales/{{lng}}/{{ns}}.missing.json'
+    },
+    fallbackLng: 'fr',
+    preload: ['fr', 'en'],
+    saveMissing: true
+  });
 
 const app = express();
+app.use(i18nextMiddleware.handle(i18next));
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
