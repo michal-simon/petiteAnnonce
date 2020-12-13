744e3afe020c86b5287884b0796a8d5ccd399961
yecodeo <yecodeo@gmail.com>
2020-12-13 02:11:12 +0100

    add slugigy function

---
+++ publication/src/helper/slugify.ts
@@ -0,0 +1,18 @@
+export default function slugify(string: string): string {
+  const a =
+    'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
+  const b =
+    'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------';
+  const p = new RegExp(a.split('').join('|'), 'g');
+
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // séparation par -
+    .replace(p, (c) => b.charAt(a.indexOf(c)))
+    .replace(/&/g, '-et-') // Replace & with 'et'
+    .replace(/[^\w-]+/g, '') // retirer tout ce qui n'est pas des lettres
+    .replace(/--+/g, '-') // remplacer des - multiple par un seul
+    .replace(/^-+/, '') // Trim - début
+    .replace(/-+$/, ''); // Trim - fin
+}
